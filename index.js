513ef18f9c307a8f7cceadc8cc7c7f0534f144a9
juliusvega998 <juliusvega998@gmail.com>
2017-12-19 15:51:27 +0800

    Now allows github.io to connect

--- index.js
+++ index.js
@@ -84,6 +84,24 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use((req, res, next) => {
+	// Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://juliusvega998.github.io');
+    
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 app.get('/fortune', function(req, res) {
 	let pos = [];
 	let msg;
